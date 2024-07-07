359a249e289c708c20d2f16c9d1e9cb92c45237f
danlawand <daniel.lawand@usp.br>
2024-07-07 12:21:50 -0300

    add backend

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify, render_template
+from flask_socketio import SocketIO
+
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# @app.route('/genai', methods=['POST'])
+# def generate_answer():
+#     data = request.json
+#     answer = data['text']
+#     return jsonify({'response': f'{answer}'})
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
